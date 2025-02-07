8b2faeb1754e6e707c3bd5c1e9065a71d1bc9844
Denisimo2024 <2132813@gmail.com>
2025-02-08 00:49:17 +0500

    VD_04_2

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from flask import Flask
-from datetime import datetime
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def show_datetime():
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return f"Текущая дата и время: {current_time}"
+def index():
+    return render_template('index.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
